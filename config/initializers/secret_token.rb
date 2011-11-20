4708e6ccbdbef3e006cd37af3bee37761f1c9104
Israel Vicars <israelvicars@gmail.com>
2011-11-20 16:06:57 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Materialmix::Application.config.secret_token = '7fec5d5dac624b4a8e31896128ed4da156af63c04204804f06925559636cf19ae7bd600da5d0990eb7ba755f4e2c6b00db43853681eec0c1bc8fbf212abd95ad'
